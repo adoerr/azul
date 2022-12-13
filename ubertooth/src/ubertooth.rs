455e1537164644875c70c8afc10b63182b30d992
adoerr <0xad@gmx.net>
2022-12-13 22:11:45 +0100

    fix Ubertooth::init()
    Signed-off-by: adoerr <0xad@gmx.net>

--- ubertooth/src/ubertooth.rs
+++ ubertooth/src/ubertooth.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
-use rusb::{Device, DeviceList, GlobalContext};
+use rusb::{Device, GlobalContext};
 
-use crate::Result;
+use crate::{Error, Result};
 
 // OpenMoko Inc
 const VENDOR: u16 = 0x1d50;
@@ -17,18 +17,14 @@ pub struct Ubertooth {
 impl Ubertooth {
     /// Initialize the [`Ubertooth`].
     pub fn init() -> Result<Ubertooth> {
-        let device = DeviceList::new()?.iter().try_find(|dev| {
+        if let Some(device) = rusb::devices()?.iter().try_find(|dev| {
             let desc = dev.device_descriptor()?;
-            if desc.vendor_id() == VENDOR && desc.product_id() == PRODUCT {
-                Ok(true)
-            } else {
-                Err(rusb::Error::NoDevice)
-            }
-        })?;
-
-        Ok(Ubertooth {
-            device: device.expect("unexpected missing device"),
-        })
+            Ok::<bool, Error>(desc.vendor_id() == VENDOR && desc.product_id() == PRODUCT)
+        })? {
+            Ok(Ubertooth { device })
+        } else {
+            Err(Error::USB(rusb::Error::NoDevice))
+        }
     }
 
     /// Return device version as `(major, minor, patch)`
