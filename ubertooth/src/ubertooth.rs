ae24ba542c7d6cec0fb20b449bee929f7684b9aa
adoerr <0xad@gmx.net>
2022-12-14 20:13:33 +0100

    use DeviceList
    Signed-off-by: adoerr <0xad@gmx.net>

--- ubertooth/src/ubertooth.rs
+++ ubertooth/src/ubertooth.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use rusb::{Device, GlobalContext};
+use rusb::{Device, DeviceList, GlobalContext};
 
 use crate::{Error, Result};
 
@@ -17,7 +17,7 @@ pub struct Ubertooth {
 impl Ubertooth {
     /// Initialize the [`Ubertooth`].
     pub fn init() -> Result<Ubertooth> {
-        if let Some(device) = rusb::devices()?.iter().try_find(|dev| {
+        if let Some(device) = DeviceList::new()?.iter().try_find(|dev| {
             let desc = dev.device_descriptor()?;
             Ok::<bool, Error>(desc.vendor_id() == VENDOR && desc.product_id() == PRODUCT)
         })? {
