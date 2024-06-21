688208a325396dd29cdc7457c25ed60600c6d051
adoerr <0xad@gmx.net>
2024-06-21 21:05:11 +0200

    gen libubertooth bindings
    Signed-off-by: adoerr <0xad@gmx.net>

--- ubertooth/build.rs
+++ ubertooth/build.rs
@@ -4,13 +4,22 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     println!("cargo:rustc-link-search=/usr/local/lib");
+
     println!("cargo:rustc-link-lib=btbb");
     println!("cargo:rerun-if-changed=btbb.h");
+
+    println!("cargo:rustc-link-lib=ubertooth");
+    println!("cargo:rerun-if-changed=ubertooth.h");
 
     bindgen::Builder::default()
         .header("/usr/local/include/btbb.h")
         .generate()?
         .write_to_file(PathBuf::from(env::var("OUT_DIR")?).join("btbb.rs"))?;
+
+    bindgen::Builder::default()
+        .header("/usr/local/include/ubertooth.h")
+        .generate()?
+        .write_to_file(PathBuf::from(env::var("OUT_DIR")?).join("ubertooth-gen.rs"))?;
 
     Ok(())
 }
