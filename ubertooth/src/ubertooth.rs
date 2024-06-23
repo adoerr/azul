136d35e6d7e3576b043b07d1ae69532dbdcaf067
adoerr <0xad@gmx.net>
2024-06-23 07:09:50 +0200

    use descriptor and handle
    Signed-off-by: adoerr <0xad@gmx.net>

--- ubertooth/src/ubertooth.rs
+++ ubertooth/src/ubertooth.rs
@@ -2,7 +2,10 @@
 
 use std::time::Duration;
 
-use rusb::{request_type, Device, DeviceList, Direction, GlobalContext, Recipient, RequestType};
+use rusb::{
+    request_type, DeviceDescriptor, DeviceHandle, DeviceList, Direction, GlobalContext, Recipient,
+    RequestType,
+};
 
 use crate::{
     usb::{Commands, Led},
@@ -16,17 +19,23 @@ const PRODUCT: u16 = 0x6002;
 
 #[derive(Debug)]
 pub struct Ubertooth {
-    device: Device<GlobalContext>,
+    descriptor: DeviceDescriptor,
+    handle: DeviceHandle<GlobalContext>,
 }
 
 impl Ubertooth {
     /// Initialize the [`Ubertooth`].
+    ///
+    /// Using the first Ubertooth Once device found.
     pub fn init() -> Result<Ubertooth> {
         if let Some(device) = DeviceList::new()?.iter().try_find(|dev| {
             let desc = dev.device_descriptor()?;
             Ok::<bool, Error>(desc.vendor_id() == VENDOR && desc.product_id() == PRODUCT)
         })? {
-            Ok(Ubertooth { device })
+            Ok(Ubertooth {
+                descriptor: device.device_descriptor()?,
+                handle: device.open()?,
+            })
         } else {
             Err(Error::USB(rusb::Error::NoDevice))
         }
@@ -34,14 +43,14 @@ impl Ubertooth {
 
     /// Return device version as `(major, minor, patch)`
     pub fn version(&self) -> Result<(u8, u8, u8)> {
-        let v = self.device.device_descriptor()?.device_version();
+        let v = self.descriptor.device_version();
         Ok((v.0, v.1, v.2))
     }
 
     pub fn user_led(&self) -> Result<u8> {
         let mut state = [0u8; 1];
 
-        self.device.open()?.read_control(
+        self.handle.read_control(
             request_type(Direction::In, RequestType::Vendor, Recipient::Endpoint),
             Commands::GET_USRLED as u8,
             0,
@@ -54,7 +63,7 @@ impl Ubertooth {
     }
 
     pub fn set_user_led(&self, state: Led) -> Result<u8> {
-        let res = self.device.open()?.write_control(
+        let res = self.handle.write_control(
             request_type(Direction::Out, RequestType::Vendor, Recipient::Endpoint),
             Commands::SET_USRLED as u8,
             state as u16,
